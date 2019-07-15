13af075886d0a8d1e1b6943723ede8fb7d402d7d
annsysadm <annsysadm@mail..ru>
2019-07-15 13:45:28 +0000

    task_17_2a done

--- exercises/17_serialization/task_17_2a.py
+++ exercises/17_serialization/task_17_2a.py
@@ -32,3 +32,45 @@
 Проверить работу параметра save_to_filename и записать итоговый словарь в файл topology.yaml.
 
 '''
+
+import glob
+import re
+import yaml
+from pprint import pprint
+
+sh_cdp_files = glob.glob('sh_cdp_n_*')
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    '''
+    The function edits command show cdp neighbor from several files.
+    :param list_of_files:
+    :param save_to_filename: Default is None
+    :return: dict and if save_to_filename is set, dict is writen to filename.YAML
+    '''
+
+    regex = re.compile(r'(?P<r_dev>\w+)  +(?P<l_intf>\S+ \S+)'
+                       r'  +\d+  +[\w ]+  +\S+ +(?P<r_intf>\S+ \S+)')
+    topology_dict = {}
+    for file in list_of_files:
+        with open(file) as f:
+            command_output = f.read()
+            connect_dict = {}
+            l_dev = re.search(r'(\S+)[>#]', command_output).group(1)
+            connect_dict[l_dev] = {}
+            for match in regex.finditer(command_output):
+                r_dev, l_intf, r_intf = match.group('r_dev', 'l_intf', 'r_intf')
+                connect_dict[l_dev][l_intf] = {r_dev: r_intf}
+            topology_dict.update(connect_dict)
+
+    if save_to_filename == None:
+        pass
+    else:
+        with open(save_to_filename, 'w') as dest:
+            yaml.dump(topology_dict, dest)
+
+
+    return topology_dict
+
+
+if __name__ == "__main__":
+    pprint(generate_topology_from_cdp(sh_cdp_files, 'topology.yaml'))
